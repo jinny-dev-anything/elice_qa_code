a4ccb973186b813a9d6617fdff79f1b9e1cbddff
jinny-dev-anything <jinny.dev.anything@gmail.com>
2025-09-07 20:19:53 +0300

    add js prj

---
+++ Javascript/script.js
@@ -0,0 +1,16 @@
+// 화면에 출력
+document.write("<h2>Hello from JavaScript!</h2>");
+
+// 콘솔에 출력 (개발자 도구 Console 탭에서 확인 가능)
+console.log("Hello, Console!");
+
+// 간단한 변수/연산 테스트
+let a = 5;
+let b = 4;
+console.log("a + b =", a + b);
+
+// 함수 테스트
+function greet(name) {
+    return "안녕하세요, " + name + "!";
+}
+console.log(greet("Elice"));
